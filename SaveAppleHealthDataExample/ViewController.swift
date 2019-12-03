57bd32ea9ad44fd066186240a6373332eb0425b7
Domenico <domeniconicoli@gmail.com>
2019-12-03 21:29:29 +0100

    Initial Commit

---
+++ SaveAppleHealthDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveAppleHealthDataExample
+//
+//  Created by Domo on 03/12/2019.
+//  Copyright © 2019 Domo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
